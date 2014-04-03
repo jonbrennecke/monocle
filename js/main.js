3e3087484b40becd77e6ae91a2e74cec428eb885
jonbrennecke <jpbrennecke@gmail.com>
2014-04-03 16:22:20 -0700

    styles styles styles

--- js/main.js
+++ js/main.js
@@ -1,36 +1,65 @@
-$(document.body).ready( function(){
+$( document ).ready( function(){
 
 
-	function Lens( img ) {
+	function Lens( img, opt ) {
 
-		// 
-		var div = $("<div id='lens-container'><div id='lens'></div></div>").appendTo( "#the-container" );
+		// parameters
+		this.img = img;
+		this.zoom = opt.zoom ? opt.zoom : 2
 
+		// create the loupe
+		this.div = $("<div id='lens-container'><div id='lens'></div></div>").appendTo( "#the-container" );
 
-		div.draggable({
+		// wait for the image to load or we'll have problems...
+		this.img.onload = function(){
 
-			containment : "parent",
+			// make it draggable
+			this.div.draggable({
 
-			create : function ( e, ui ) {
+				containment : "parent",
+				create : this.create.bind( this ),
+				drag : this.drag.bind( this )
 
-				// create the magnified image
-				div.append( "<div id='lens-image-container'><img id='lens-image' src='" + img.src + "' /></div>" );
-			
-			},
+			}).css("position", "absolute");
 
-			drag : function ( e, ui ) {
-
-				$( '#lens-image' ).offset({
-					top : -ui.offset.top - 50,
-					left : -ui.offset.left - 100 
-				});
-
-			}
-		});
+		}.bind(this)
 	};
 
 	Lens.prototype = {
 
+		/**
+		 *
+		 *
+		 */
+		create : function ( e, ui ) {
+
+			// create the loupe image
+			var loupe = $( "<div id='lens-image-container'><img id='lens-image' src='" + this.img.src + "' /></div>" )
+				.appendTo( this.div )
+				.find('img#lens-image')
+
+			// set the width to 1x magnification
+			loupe.width( $( this.img ).width() + "px" );
+
+			// animate zooming in (height is auto)
+			loupe.animate({ width : ( $(this.img).width() * this.zoom ) + "px" }, { duration : 1000, easing : "easeInOutQuad" });
+		
+		},
+
+
+		/**
+		 *
+		 * @param e - event object 
+		 * @param ui - 
+		 */
+		drag : function ( e, ui ) {
+
+			$( '#lens-image' ).offset({
+				top : - ( ui.offset.top * this.zoom - ui.offset.top ),
+				left : - ( ui.offset.left * this.zoom - ui.offset.left ) 
+			});
+ 
+		}
 	};
 
 
@@ -38,28 +67,31 @@ $(document.body).ready( function(){
 	 *
 	 * Convex Lens magnifier
 	 *
+	 *
+	 * @param json array opt - array of options
+	 *
 	 */
-	$.fn.convex = function ( ) {
+	$.fn.convex = function ( opt ) {
 
-		return $( this ).each( function () {
+		return $( this ).each( function ( ) {
 
 			// first check if 'this' is an image tag
 			if ( this.tagName === "IMG" && this.nodeName === "IMG" ) {
 
 				// determine if the lens already exists
 				if ( !$("#lens").length ) {
-					var lens = new Lens( this );
+					var lens = new Lens( this, opt );
 				}
 
 			}
 			// TODO background image
 
-
-			// document.body.append()
 		});
 	}
 
-	$("#the-image").convex();
+	$("#the-image").convex({
+		zoom : 1.5
+	});
 
 
 });
